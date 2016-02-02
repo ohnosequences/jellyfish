02f5e6c03d30ebb824147dfe9e261a70a5cbcec9
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-02-02 16:46:20 +0100

    add apply method for jellyfish query

--- src/main/scala/api/commands/query.scala
+++ src/main/scala/api/commands/query.scala
@@ -14,6 +14,12 @@ case object query extends JellyfishCommand {
     |[AnyJellyfishOption]
   )
 
+  type ArgumentsVals =
+    (input.type := input.Raw)   ::
+    (mers.type := mers.Raw)     ::
+    (output.type := output.Raw) ::
+    *[AnyDenotation]
+
   type Options = options.type
   case object options extends RecordType(
     load      :×:
@@ -21,9 +27,21 @@ case object query extends JellyfishCommand {
     |[AnyJellyfishOption]
   )
 
+  type OptionsVals =
+    (load.type := load.Raw)       ::
+    (no_load.type := no_load.Raw) ::
+    *[AnyDenotation]
+
   lazy val defaults = options(
     load(false)     ::
     no_load(false)  ::
     *[AnyDenotation]
   )
+
+  def apply(
+    argumentValues: ArgumentsVals,
+    optionValues: OptionsVals
+  )
+  : JellyfishExpression[query.type, ArgumentsVals, OptionsVals] =
+    JellyfishExpression(query)(arguments := argumentValues, options := optionValues)
 }
--- src/main/scala/api/commands/queryAll.scala
+++ src/main/scala/api/commands/queryAll.scala
@@ -6,6 +6,7 @@ import better.files._
 
 case object queryAll extends JellyfishCommand {
 
+  // NOTE this is important, don't touch it
   override lazy val name = Seq("jellyfish", "query")
 
   type Arguments = arguments.type
@@ -16,6 +17,12 @@ case object queryAll extends JellyfishCommand {
     |[AnyJellyfishOption]
   )
 
+  type ArgumentsVals =
+    (input.type := input.Raw)   ::
+    (sequence.type := sequence.Raw)     ::
+    (output.type := output.Raw) ::
+    *[AnyDenotation]
+
   type Options = options.type
   case object options extends RecordType(
     load      :×:
@@ -23,9 +30,21 @@ case object queryAll extends JellyfishCommand {
     |[AnyJellyfishOption]
   )
 
+  type OptionsVals =
+    (load.type := load.Raw)       ::
+    (no_load.type := no_load.Raw) ::
+    *[AnyDenotation]
+
   lazy val defaults = options(
     load(false)     ::
     no_load(false)  ::
     *[AnyDenotation]
   )
+
+  def apply(
+    argumentValues: ArgumentsVals,
+    optionValues: OptionsVals
+  )
+  : JellyfishExpression[queryAll.type, ArgumentsVals, OptionsVals] =
+    JellyfishExpression(queryAll)(arguments := argumentValues, options := optionValues)
 }
